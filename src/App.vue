094a22eb77e33a85cc8aad2c155817910de5dd52
Dark_Mowah <antoine.coupat@neuf.fr>
2021-01-03 16:02:58 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+    <div>
+      <b-button v-b-toggle.collapse-1 variant="primary"
+        >Toggle Collapse</b-button
+      >
+      <b-collapse id="collapse-1" class="mt-2">
+        <b-card>
+          <p class="card-text">Collapse contents Here</p>
+          <b-button v-b-toggle.collapse-1-inner size="sm"
+            >Toggle Inner Collapse</b-button
+          >
+          <b-collapse id="collapse-1-inner" class="mt-2">
+            <b-card>Hello!</b-card>
+          </b-collapse>
+        </b-card>
+      </b-collapse>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
