2678fb38b0e022182bfdfc5dfa192641a2c358d7
Dark_Mowah <antoine.coupat@neuf.fr>
2021-01-13 21:54:59 +0100

    Fetching projs (WIP)

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,3 +1,4 @@
+
 import Component from "vue-class-component";
 import Vue from "vue";
 import './home.scss'
@@ -6,8 +7,17 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 @Component({
     template: require('./home.html')
 })
-export class Home extends Vue{
+export class Home extends Vue {
 
     private menuIcon = faBars;
+    private projs: any = []
+
+    public mounted() {
+        fetch('http://localhost:8080/api/proj')
+            .then(async response => {
+                const res = await response.json();
+                this.projs =  res.content;
+            })
+    }
 
 }
