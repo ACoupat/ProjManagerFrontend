123de634877943f62481532d21721d65693a59df
Dark_Mowah <antoine.coupat@neuf.fr>
2021-01-17 12:46:50 +0100

    Call backend from ip address rather than 'localhost'

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -15,7 +15,8 @@ export class Home extends Vue {
     private projs: any = []
 
     public mounted() {
-        fetch('http://localhost:8080/api/proj')
+        // fetch('http://localhost:8080/api/proj')
+        fetch('http://192.168.1.6:8080/api/proj')
             .then(async response => {
                 const res = await response.json();
                 this.projs =  res.content;
