92257d7eb1511d568969e303303f0cc9abf71141
Dark_Mowah <antoine.coupat@neuf.fr>
2021-04-10 20:39:50 +0200

    New fields in proj

--- src/components/fields/switchable-field/index.ts
+++ src/components/fields/switchable-field/index.ts
@@ -2,7 +2,7 @@ import './switchable-field.scss'
 import Component from 'vue-class-component';
 import Vue from 'vue';
 @Component({
-    props: ['editMode', 'label'],
+    props: ['editMode', 'label', 'textarea'],
     template: require('./switchable-field.html')
 })
 export class SwitchableField extends Vue {
@@ -10,6 +10,7 @@ export class SwitchableField extends Vue {
     private value : any = "";
     public editMode: boolean;
     public label: string;
+    public textarea : boolean;
 
     private onInput(event : any){
         this.value = event;
